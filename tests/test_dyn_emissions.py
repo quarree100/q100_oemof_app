5714da07119b92e4a2041a60baacab2349b80992
Johannes Roeder <johannes.roeder@uni-bremen.de>
2019-02-19 19:05:01 +0100

    add dynamic emission example

---
+++ tests/test_dyn_emissions.py
@@ -0,0 +1,88 @@
+from oemof.tools import logger
+import oemof.solph as solph
+import oemof.outputlib as outputlib
+import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+from customized import add_contraints
+
+
+def test_dyn_emission_example():
+    logger.define_logging()
+    logging.info('Initialize the energy system')
+
+    number_timesteps = 11
+
+    date_time_index = pd.date_range('1/1/2012', periods=number_timesteps, freq='H')
+
+    es = solph.EnergySystem(timeindex=date_time_index)
+
+    # Create oemof objects
+    logging.info('Create oemof objects')
+
+    # create electricity bus
+    bel = solph.Bus(label='bel')
+
+    # add bgas and bel to energysystem
+    es.add(bel)
+
+    # create source object representing the electricity net
+    elec_fossil = solph.Source(label='elec_net_fossil',
+                               outputs={bel: solph.Flow(variable_costs=10)})
+
+    # create source object representing the electricity net
+    es.add(solph.Source(label='elec_net_green',
+                        outputs={bel: solph.Flow(variable_costs=30)}))
+
+    # create simple sink object representing the electrical demand
+    es.add(solph.Sink(label='demand_el',
+                      inputs={bel: solph.Flow(
+                          actual_value=[5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5],
+                          fixed=True, nominal_value=1)}))
+
+    es.add(elec_fossil)
+
+    # Optimise the energy system
+    logging.info('Optimise the energy system')
+
+    # initialise the operational model
+    om = solph.Model(energysystem=es)
+
+    # add specific emission values as a list to flow objects
+    for i, o in om.flows.keys():
+        if i is bel:
+            om.flows[i, o].emission_factor =\
+                [0.01 for i in range(0, number_timesteps)]
+        if i is elec_fossil:
+            om.flows[i, o].emission_factor =\
+                [0.7, 0.7, 0.7, 0.7, 0.1, 0.5, 1, 1.3, 0.7, 0.7, 0.7]
+
+    add_contraints.emission_limit_dyn(om, limit=15)
+
+    # Ausgabe der verwendeten Emissions Faktoren:
+    for (i, o) in om.flows:
+        if hasattr(om.flows[i, o], 'emission_factor'):
+            print('EMISSIONS')
+            print(i, o, om.flows[i, o].emission_factor)
+
+    # if tee_switch is true solver messages will be displayed
+    logging.info('Solve the optimization problem')
+    om.solve(solver='cbc', solve_kwargs={'tee': False})
+
+    # Check and plot the results
+    results = outputlib.processing.results(om)
+
+    # get all variables of a specific component/bus
+    electricity_bus = outputlib.views.node(results, 'bel')["sequences"]
+
+    # plot the time series (sequences) of a specific component/bus
+    if plt is not None:
+        ax = electricity_bus.plot(kind='line', drawstyle='steps-mid',
+                                  legend='right')
+        plt.show()
+
+    # print total emissions
+    print('Total emissions:')
+    print(om.total_emissions())
+
+test_dyn_emission_example()
